67d3856853ab4f33735a357e0f79da31c475014a
Sye-5pace <owusilasyp00@gmail.com>
2024-06-09 22:15:20 +0100

    :sparkles: Feat: attach selectors to reducers for state management

--- src/app/store/Comment/comment.selectors.ts
+++ src/app/store/Comment/comment.selectors.ts
@@ -1,2 +1,19 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { IState } from './comment.reducer'
 
+export const selectCommentFeature = createFeatureSelector<IState>('commentInfo')
+
+export const selectCommentInfo = createSelector(
+  selectCommentFeature,
+  (state: IState) => state.commentInfo
+);
+
+export const selectLoadCommentSuccess = createSelector(
+  selectCommentFeature,
+  (state: IState) => state.laoding
+)
+
+export const selectLoadCommentFailure = createSelector(
+  selectCommentFeature,
+  (state: IState) => state.error
+)
